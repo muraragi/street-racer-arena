f96f432a20b8625b46288a629b3346da653388c5
murargi <muraragi99@gmail.com>
2025-05-14 14:26:20 +0300

    attempt to fix env

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/fonts', 'shadcn-nuxt', '@nuxtjs/seo'],
   fonts: {
     families: [
@@ -15,7 +14,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiUrl: process.env.NUXT_API_URL
+      apiUrl: ''
     }
   },
   shadcn: {
